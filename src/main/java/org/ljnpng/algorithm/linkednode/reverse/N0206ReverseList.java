9ca99258a0810a7165a73e35823b53496a8a5d5a
neo.liao <ljpfuture@gmail.com>
2022-12-21 21:22:02 +0800

    algorithm-20 206.反转链表

---
+++ src/main/java/org/ljnpng/algorithm/linkednode/reverse/N0206ReverseList.java
@@ -0,0 +1,57 @@
+package org.ljnpng.algorithm.linkednode.reverse;
+
+import org.ljnpng.algorithm.domain.ListNode;
+
+public class N0206ReverseList {
+
+    /**
+     * 使用迭代解决反转链表
+     * 假设 1 -> 2 -> 3 -> 4
+     * @param head 链表头
+     * @return 新的链表头
+     *
+     */
+    public ListNode reverseList(ListNode head) {
+        // pre = null, cur = 1
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            // next = 2
+            ListNode next = cur.next;
+            // 1 -> 2 ==> 1 -> null
+            cur.next = pre;
+            // pre = 1
+            pre = cur;
+            // cur = 2
+            cur = next;
+        }
+        return pre;
+   }
+
+    /**
+     *
+     * 使用递归解决
+     * 假设 1 -> 2 -> 3 -> 4
+     * @param head 头节点
+     * @return 反转后的节点，即尾节点
+     *
+     */
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            // head == null 用于快速筛选出 [] 空链表的情况
+            // 实际用于判断是 head.next == null,  相当于一直递归找到尾节点
+            return head;
+        }
+
+        // reverseList2 递归到底后返回的就是尾节点 也就是 newHead = 4
+        ListNode newHead = reverseList2(head.next);
+
+        // head 就是倒数第二个节点，下面这一句 相当于  3 -> 4 -> null ==> 3 <-> 4
+        head.next.next = head;
+
+        // 3 <-> 4  ==> null <- 3 <- 4
+        head.next = null;
+
+        // return 4
+        return newHead;
+    }
+}
---
+++ src/test/java/org/ljnpng/algorithm/linkednode/reverse/N0206ReverseListTest.java
@@ -0,0 +1,42 @@
+package org.ljnpng.algorithm.linkednode.reverse;
+
+import org.junit.jupiter.api.Test;
+import org.ljnpng.algorithm.domain.ListNode;
+import org.ljnpng.algorithm.util.Utils;
+import sun.text.normalizer.UTF16;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class N0206ReverseListTest {
+
+    N0206ReverseList n0206ReverseList = new N0206ReverseList();
+
+    @Test
+    void testEmptyListNode() {
+        ListNode empty = Utils.stringToListNode("[]");
+
+        assertEquals(empty, n0206ReverseList.reverseList(empty));
+        assertEquals(empty, n0206ReverseList.reverseList2(empty));
+
+    }
+
+    @Test
+    void testSingleListNode() {
+        ListNode singleton = Utils.stringToListNode("[1]");
+
+        assertEquals(singleton, n0206ReverseList.reverseList(singleton));
+        assertEquals(singleton, n0206ReverseList.reverseList2(singleton));
+    }
+
+    @Test
+    void testMultipleListNode() {
+        ListNode multiple = Utils.stringToListNode("[1,2,3,4]");
+        ListNode reverse = Utils.stringToListNode("[4,3,2,1]");
+
+        assertEquals(reverse, n0206ReverseList.reverseList(multiple));
+
+        multiple = Utils.stringToListNode("[1,2,3,4]");
+        assertEquals(reverse, n0206ReverseList.reverseList2(multiple));
+    }
+
+}
