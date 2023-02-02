512809704ab76407fcba379aca4e9114f215640e
neo.liao <ljpfuture@gmail.com>
2023-02-02 23:55:34 +0800

    algorithm-63 104.二叉树的最大深度

---
+++ src/main/java/org/ljnpng/algorithm/domain/TreeNode.java
@@ -0,0 +1,12 @@
+package org.ljnpng.algorithm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+}
---
+++ src/main/java/org/ljnpng/algorithm/tree/N104MaxDepth.java
@@ -0,0 +1,13 @@
+package org.ljnpng.algorithm.tree;
+
+
+import org.ljnpng.algorithm.domain.TreeNode;
+
+public class N104MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
