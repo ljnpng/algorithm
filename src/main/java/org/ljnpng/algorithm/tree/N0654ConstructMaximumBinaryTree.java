c7a168b3cad4516467495b284be67e7b8b35fa55
neo.liao <ljpfuture@gmail.com>
2023-02-17 00:32:49 +0800

    :sparkles: #78 654.最大二叉树

---
+++ src/main/java/org/ljnpng/algorithm/tree/N0654ConstructMaximumBinaryTree.java
@@ -0,0 +1,47 @@
+package org.ljnpng.algorithm.tree;
+
+import org.ljnpng.algorithm.domain.TreeNode;
+
+public class N0654ConstructMaximumBinaryTree {
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        int index = maxIndex(nums, 0, nums.length - 1);
+        TreeNode left = constructMaxTree(nums, 0, index - 1);
+        TreeNode right = constructMaxTree(nums, index + 1, nums.length - 1);
+        return new TreeNode(nums[index], left, right);
+    }
+
+    private TreeNode constructMaxTree(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        if (left == right) {
+            return new TreeNode(nums[left]);
+        }
+
+        int max = Integer.MIN_VALUE;
+        int inx = left;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] > max) {
+                inx = i;
+                max = nums[i];
+            }
+        }
+        return new TreeNode(nums[inx], constructMaxTree(nums, left, inx - 1), constructMaxTree(nums, inx + 1, right));
+    }
+
+    private int maxIndex(int[] nums, int left, int right) {
+        int max = Integer.MIN_VALUE;
+        int inx = left;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] > max) {
+                inx = i;
+                max = nums[i];
+            }
+        }
+        return inx;
+    }
+}
