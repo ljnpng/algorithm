70154d00a43506564c4d443f4f7021614e241ca3
neo.liao <ljpfuture@gmail.com>
2023-02-19 21:49:06 +0800

    :zap: #80 105.从前序与中序遍历序列构造二叉树 enhance with hashmap to find inx

--- src/main/java/org/ljnpng/algorithm/tree/N0105BuildTree.java
+++ src/main/java/org/ljnpng/algorithm/tree/N0105BuildTree.java
@@ -2,25 +2,28 @@ package org.ljnpng.algorithm.tree;
 
 import org.ljnpng.algorithm.domain.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class N0105BuildTree {
 
     private int inx = -1;
 
+    Map<Integer, Integer> valToIndex;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if (preorder.length == 0) return null;
+        valToIndex = new HashMap<>(inorder.length);
+        for (int i = 0; i < inorder.length; i++) {
+            valToIndex.put(inorder[i], i);
+        }
         return buildTree(preorder, inorder, 0, inorder.length - 1);
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder, int low, int high) {
         if (low > high) return null;
         TreeNode root = new TreeNode(preorder[++inx]);
-        int cur = -1;
-        for (int i = low; i <= high; i++) {
-            if (inorder[i] == preorder[inx]) {
-                cur = i;
-                break;
-            }
-        }
+        int cur = valToIndex.get(preorder[inx]);
         root.left = buildTree(preorder, inorder, low, cur - 1);
         root.right = buildTree(preorder, inorder, cur + 1, high);
         return root;
