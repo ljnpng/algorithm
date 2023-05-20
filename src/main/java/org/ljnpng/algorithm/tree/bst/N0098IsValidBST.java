c6f3f36ae86afe1214245ab8b15a9e4685823d04
neo.liao <ljpfuture@gmail.com>
2023-05-20 16:37:30 +0800

    #104 优化入场,使用 Long.MIN_VALUE 和 Long.MXA_VALUE 去掉对null的判断

--- src/main/java/org/ljnpng/algorithm/tree/bst/N0098IsValidBST.java
+++ src/main/java/org/ljnpng/algorithm/tree/bst/N0098IsValidBST.java
@@ -4,17 +4,18 @@ import org.ljnpng.algorithm.domain.TreeNode;
 
 public class N0098IsValidBST {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    private boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+    private boolean isValidBST(TreeNode root, long min, long max) {
         if (root == null) {
             return true;
         }
-        if (min != null && min.val >= root.val) return false;
-        if (max != null && max.val <= root.val) return false;
-        return isValidBST(root.left, min, root)
-                && isValidBST(root.right, root, max);
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValidBST(root.left, min, root.val)
+                && isValidBST(root.right, root.val, max);
     }
 
 }
