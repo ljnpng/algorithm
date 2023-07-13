e5ea5e6d37749206fbc83b24e9a8a3a4f4e56665
neo <ljpfuture@gmail.com>
2023-07-13 23:41:21 +0800

    :tada: close #111 931.下降路径最小和 自认为优化的负优化

---
+++ src/main/java/org/ljnpng/algorithm/dp/MinFallingPathSum.java
@@ -0,0 +1,22 @@
+package org.ljnpng.algorithm.dp;
+
+import java.util.Arrays;
+
+public class MinFallingPathSum {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        // 初始化第一行
+        System.arraycopy(matrix[0], 0, dp[0], 0, n);
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]) + matrix[i][0];
+            for (int j = 1; j < n - 1; j++) {
+                dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i - 1][j + 1]) + matrix[i][j];
+            }
+            dp[i][n - 1] = Math.min(dp[i - 1][n - 2], dp[i - 1][n - 1]) + matrix[i][n - 1];
+        }
+        return Arrays.stream(dp[n - 1]).min().orElseThrow();
+    }
+
+}
+
---
+++ src/test/java/org/ljnpng/algorithm/dp/MinFallingPathSumTest.java
@@ -0,0 +1,28 @@
+package org.ljnpng.algorithm.dp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MinFallingPathSumTest {
+
+    @Test
+    void minFallingPathSum() {
+        int[][] A = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        int result = new MinFallingPathSum().minFallingPathSum(A);
+        assertEquals(12, result);
+
+        // n = 1 边界
+        A = new int[][]{{1}};
+        result = new MinFallingPathSum().minFallingPathSum(A);
+        assertEquals(1, result);
+
+        // n = 2 边界
+        A = new int[][]{{1,2},{3,4}};
+        result = new MinFallingPathSum().minFallingPathSum(A);
+        assertEquals(4, result);
+
+
+    }
+
+}
