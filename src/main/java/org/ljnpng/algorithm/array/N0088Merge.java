8fe298d11eb795558208d1478771e2ce7fb10a34
neo <ljpfuture@gmail.com>
2023-07-19 01:15:42 +0800

    enhance #115 88.合并两个有序数组 空间优化, 去掉中间数组, 时间也优化了一点

--- src/main/java/org/ljnpng/algorithm/array/N0088Merge.java
+++ src/main/java/org/ljnpng/algorithm/array/N0088Merge.java
@@ -2,21 +2,19 @@ package org.ljnpng.algorithm.array;
 
 public class N0088Merge {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] tmp = new int[m];
-        System.arraycopy(nums1, 0, tmp, 0, m);
-        int i = 0, j = 0, p = 0;
-        while (i < m && j < n) {
-            if (tmp[i] <= nums2[j]) {
-                nums1[p++] = tmp[i++];
+        int i = m - 1, j = n - 1, p = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] <= nums2[j]) {
+                nums1[p--] = nums2[j--];
             } else {
-                nums1[p++] = nums2[j++];
+                nums1[p--] = nums1[i--];
             }
         }
-        while (i < m) {
-            nums1[p++] = tmp[i++];
+        while (i >= 0) {
+            nums1[p--] = nums1[i--];
         }
-        while (j < n) {
-            nums1[p++] = nums2[j++];
+        while (j >= 0) {
+            nums1[p--] = nums2[j--];
         }
     }
 }
