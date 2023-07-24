82e7b82f71169bfdb75bae262c2ad1addda5adc4
neo <ljpfuture@gmail.com>
2023-07-24 20:54:12 +0800

    simplify #123 55.跳跃游戏

--- src/main/java/org/ljnpng/algorithm/array/N0055CanJump.java
+++ src/main/java/org/ljnpng/algorithm/array/N0055CanJump.java
@@ -2,24 +2,19 @@ package org.ljnpng.algorithm.array;
 
 public class N0055CanJump {
     public boolean canJump(int[] nums) {
-        if (nums.length <= 1) {
-            return true;
-        }
-
-        int maxStep = nums[0];
+        int maxStep = 0;
         int n = nums.length;
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             if (i <= maxStep) {
                 maxStep = Math.max(maxStep, nums[i] + i);
-                if (maxStep >=n) {
+                if (maxStep >= n - 1) {
                     return true;
                 }
             } else {
                 return false;
             }
-
         }
-        return true;
+        return false;
 
     }
 }
