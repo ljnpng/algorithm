2bb02e4c7b90c69e4341f29884433d49e25e802a
neo <ljpfuture@gmail.com>
2023-08-07 00:07:00 +0800

    enhance #142 3.无重复字符的最长子串 对第一commit 使用hash的优化

--- src/main/java/org/ljnpng/algorithm/window/N0003LengthOfLongestSubstring.java
+++ src/main/java/org/ljnpng/algorithm/window/N0003LengthOfLongestSubstring.java
@@ -1,18 +1,20 @@
 package org.ljnpng.algorithm.window;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class N0003LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
         int left = 0, right = 0;
         int longest = 0;
         int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
         while (right < n) {
-            char cur = s.charAt(right);
-            for (int i = left; i < right; i++) {
-                if (s.charAt(i) == cur) {
-                    left = i + 1;
-                    break;
-                }
+            Integer i = map.get(s.charAt(right));
+            if (i != null) {
+                left = Math.max(i + 1, left); // 有可能重复元素在left 前面
             }
+            map.put(s.charAt(right), right); // 重新更新下标
             right++;
             longest = Math.max(longest, right - left);
         }
