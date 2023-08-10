572ba88280333423e0d6aac8d4076a3df0b3ad86
neo <ljpfuture@gmail.com>
2023-08-10 22:48:07 +0800

    enhance #146 73.矩阵置零

--- src/main/java/org/ljnpng/algorithm/array/two/N0073SetZeros.java
+++ src/main/java/org/ljnpng/algorithm/array/two/N0073SetZeros.java
@@ -1,37 +1,28 @@
 package org.ljnpng.algorithm.array.two;
 
-import java.math.BigInteger;
-
 public class N0073SetZeros {
 
     public void setZeroes(int[][] matrix) {
-        // 只有 = 0 or != 0 两种 情况, 所以可以用二进制标识
-        BigInteger row = BigInteger.ZERO;
-        BigInteger col = BigInteger.ZERO;
         int n = matrix.length, m = matrix[0].length;
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    row = row.or(BigInteger.ONE.shiftLeft(i));
-                    col = col.or(BigInteger.ONE.shiftLeft(j));
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
 
         for (int i = 0; i < n; i++) {
-            if (row.testBit(i)) {
-                for (int j = 0; j < m; j++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
             }
+
         }
 
-        for (int i = 0; i < m; i++) {
-            if (col.testBit(i)) {
-                for (int j = 0; j < n; j++) {
-                    matrix[j][i] = 0;
-                }
-            }
-        }
     }
 }
