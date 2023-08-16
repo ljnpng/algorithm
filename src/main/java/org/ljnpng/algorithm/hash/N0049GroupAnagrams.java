1653056196637f3fefe407c44e32c041cc33f021
neo <ljpfuture@gmail.com>
2023-08-16 21:02:08 +0800

    enhance char 比 String 快 #152 49.字母异位词分组

--- src/main/java/org/ljnpng/algorithm/hash/N0049GroupAnagrams.java
+++ src/main/java/org/ljnpng/algorithm/hash/N0049GroupAnagrams.java
@@ -1,47 +1,21 @@
 package org.ljnpng.algorithm.hash;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class N0049GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        for (String str : strs) {
-            int j = 0;
-            for (; j < result.size(); j++) {
-                if (isAnagram(result.get(j).get(0), str)) {
-                    result.get(j).add(str);
-                    break;
-                }
-            }
-            if (j == result.size()) {
-                List<String> obj = new ArrayList<>();
-                obj.add(str);
-                result.add(obj);
-            }
-        }
-        return result;
+        return new ArrayList<>(Arrays.stream(strs)
+//                .collect(Collectors.groupingBy(s -> Arrays.stream(s.split("")).sorted().collect(Collectors.joining(""))))
+                .collect(Collectors.groupingBy(s -> {
+                    char[] chars = s.toCharArray();
+                    Arrays.sort(chars);
+                    return new String(chars);
+                }))
+                .values());
     }
 
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        int[] words = new int[26];
-        int len = s.length();
-        for (int i = 0; i < len; i++) {
-            char c = s.charAt(i);
-            words[c - 'a']++;
-        }
-        for (int i = 0; i < len; i++) {
-            char c = t.charAt(i);
-            if (words[c - 'a'] == 0) {
-                return false;
-            }
-            words[c - 'a']--;
-        }
-
-        return true;
-    }
 
 }
