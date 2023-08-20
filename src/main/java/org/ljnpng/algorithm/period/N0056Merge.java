7b22a6a199d1cb2906aa690c826883ffa3fd1ad4
neo <ljpfuture@gmail.com>
2023-08-20 16:22:20 +0800

    enhance #157 56.合并区间

--- src/main/java/org/ljnpng/algorithm/period/N0056Merge.java
+++ src/main/java/org/ljnpng/algorithm/period/N0056Merge.java
@@ -1,26 +1,26 @@
 package org.ljnpng.algorithm.period;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class N0056Merge {
 
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparing(ints -> ints[0]));
         int n = intervals.length;
-        int cur = 0;
+        List<int[]> res = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             int low = intervals[i][0];
             int high = intervals[i][1];
 
-            while (i + 1 < n && intervals[i + 1][0] >= low && intervals[i + 1][0] <= high) {
+            while (i + 1 < n && intervals[i + 1][0] <= high) {
                 high = Math.max(intervals[i + 1][1], high);
                 i++;
             }
-            intervals[cur][0] = low;
-            intervals[cur][1] = high;
-            cur++;
+            res.add(new int[]{low, high});
         }
-        return Arrays.copyOf(intervals, cur);
+        return res.toArray(new int[][]{});
     }
 }
