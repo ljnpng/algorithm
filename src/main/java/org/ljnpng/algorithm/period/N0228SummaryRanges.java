30b6d52a80fec824be2966dfdbca907f2d1c83bd
neo <ljpfuture@gmail.com>
2023-08-20 15:32:55 +0800

    enhance #156 228.汇总区间

--- src/main/java/org/ljnpng/algorithm/period/N0228SummaryRanges.java
+++ src/main/java/org/ljnpng/algorithm/period/N0228SummaryRanges.java
@@ -1,30 +1,27 @@
 package org.ljnpng.algorithm.period;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class N0228SummaryRanges {
     public List<String> summaryRanges(int[] nums) {
-        if (nums.length == 0) {
-            return Collections.emptyList();
-        }
-        if (nums.length == 1) {
-            return Collections.singletonList(String.valueOf(nums[0]));
-        }
-
-        List<String> res = new ArrayList<>();
         int start = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if ((long)nums[i] - nums[i - 1] > 1L) {
-                String s =  i - 1 == start ? String.valueOf(nums[start]) : nums[start] + "->" + nums[i - 1];
-                res.add(s);
-                start = i;
-            }
-        }
+        int n = nums.length;
+        List<String> res = new ArrayList<>();
 
-        String s =  nums.length - 1 == start ? String.valueOf(nums[start]) : nums[start] + "->" + nums[nums.length - 1];
-        res.add(s);
+        while (start < n) {
+            int low = start;
+            while (start + 1 < n && nums[start + 1] == nums[start] + 1) {
+                start++;
+            }
+
+            StringBuilder sb = new StringBuilder().append(nums[low]);
+            if (start != low) {
+                sb.append("->").append(nums[start]);
+            }
+            res.add(sb.toString());
+            start++;
+        }
         return res;
     }
 }
