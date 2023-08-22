e1a8290a7e12b4f8cc756211b4618c81d1d09148
neo <ljpfuture@gmail.com>
2023-08-22 13:31:16 +0800

    close #162 155.最小栈

---
+++ src/main/java/org/ljnpng/algorithm/stack/N0155MinStack.java
@@ -0,0 +1,43 @@
+package org.ljnpng.algorithm.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class N0155MinStack {
+
+    private Deque<StackValue> stack;
+
+    public N0155MinStack() {
+        stack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        Integer min = stack.isEmpty() ? val : Math.min(stack.peek().min, val);
+        stack.push(new StackValue(val, min));
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek().value;
+    }
+
+    public int getMin() {
+        return stack.peek().min;
+
+    }
+
+    private static class StackValue {
+        Integer value;
+
+        Integer min;
+
+        StackValue(Integer value, Integer min) {
+            this.value = value;
+            this.min = min;
+        }
+    }
+
+}
