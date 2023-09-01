eb2b8aed77ffb3f5a67dc199e0daaeb0b2b28dbd
neo <ljpfuture@gmail.com>
2023-09-01 17:00:47 +0800

    add bfs #184 200.岛屿数量

--- src/main/java/org/ljnpng/algorithm/graphy/NumIslands.java
+++ src/main/java/org/ljnpng/algorithm/graphy/NumIslands.java
@@ -1,5 +1,8 @@
 package org.ljnpng.algorithm.graphy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class NumIslands {
 
     /**
@@ -20,7 +23,8 @@ public class NumIslands {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+//                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                     count++;
                 }
             }
@@ -42,6 +46,27 @@ public class NumIslands {
         dfs(grid, i, j + 1);
         dfs(grid, i - 1, j);
         dfs(grid, i, j - 1);
+    }
+
+    private void bfs(char[][] grid, int i, int j) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j});
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
+            if (x < nr && y < nc && x >= 0 && y >= 0 && grid[x][y] == '1') {
+                grid[x][y] = '0';
+                queue.offer(new int[]{x + 1, y});
+                queue.offer(new int[]{x, y + 1});
+                queue.offer(new int[]{x - 1, y});
+                queue.offer(new int[]{x, y - 1});
+            }
+        }
+
     }
 
 
