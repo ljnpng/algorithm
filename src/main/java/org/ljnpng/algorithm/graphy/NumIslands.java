8f2f6d368e524c7de75f4d2ab2cf90907ff0d22a
neo <ljpfuture@gmail.com>
2023-09-01 16:36:30 +0800

    close #184 200.岛屿数量

---
+++ src/main/java/org/ljnpng/algorithm/graphy/NumIslands.java
@@ -0,0 +1,48 @@
+package org.ljnpng.algorithm.graphy;
+
+public class NumIslands {
+
+    /**
+     * dfs
+     *
+     * @param grid
+     * @return
+     */
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (i >= nr || j >= nc || i < 0 || j < 0 || grid[i][j] == '0') {
+            return;
+        }
+
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+
+
+}
