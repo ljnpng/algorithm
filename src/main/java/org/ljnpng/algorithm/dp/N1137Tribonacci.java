7bb5132acdddf7f14952ab3ba93c16930ef376d4
neo <ljpfuture@gmail.com>
2023-09-25 13:46:42 +0800

    enhance #204 1137. 第 N 个泰波那契数

--- src/main/java/org/ljnpng/algorithm/dp/N1137Tribonacci.java
+++ src/main/java/org/ljnpng/algorithm/dp/N1137Tribonacci.java
@@ -2,9 +2,6 @@ package org.ljnpng.algorithm.dp;
 
 public class N1137Tribonacci {
     public int tribonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 1;
         int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 1;
