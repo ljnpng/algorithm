f8766c6db7f1eb5494f243d797e9ca77ac710d81
neo <ljpfuture@gmail.com>
2023-10-14 16:02:51 +0800

    close #207 433.最小基因变化

---
+++ src/main/java/org/ljnpng/algorithm/bfs/MinMutation.java
@@ -0,0 +1,47 @@
+package org.ljnpng.algorithm.bfs;
+
+import java.util.*;
+
+public class MinMutation {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Set<String> genes = new HashSet<>(bank.length);
+        genes.addAll(Arrays.asList(bank));
+        if (!genes.contains(endGene)) {
+            return -1;
+        }
+
+        Queue<String> queue = new java.util.LinkedList<>();
+        queue.offer(startGene);
+        int step = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String gene = queue.poll();
+                if (gene.equals(endGene)) {
+                    return step;
+                }
+                Iterator<String> iterator = genes.iterator();
+                while (iterator.hasNext()) {
+                    String s = iterator.next();
+                    if (isMutation(gene, s)) {
+                        queue.offer(s);
+                        iterator.remove();
+                    }
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+
+    private boolean isMutation(String s1, String s2) {
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+            }
+        }
+        return diff == 1;
+    }
+
+}
